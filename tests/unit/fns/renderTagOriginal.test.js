6b8b4ed2b6b3a2ccd243057b1142ec75c3ceb2e2
homer0 <me@homer0.dev>
2024-04-09 03:20:15 +0200

    chore: add fn to render a tag on its original format

---
+++ tests/unit/fns/renderTagOriginal.test.js
@@ -0,0 +1,48 @@
+jest.unmock('../../../src/fns/renderTagOriginal');
+
+const { renderTagOriginal } = require('../../../src/fns/renderTagOriginal');
+
+describe('renderTagOriginal', () => {
+  const cases = [
+    {
+      it: 'should render a single line tag',
+      input: {
+        source: [{ source: '* This is a single line tag.' }],
+      },
+      output: ['This is a single line tag.'],
+    },
+    {
+      it: 'should render a multiline tag',
+      input: {
+        source: [
+          { source: ' * @param {string} name This is a multiline tag.' },
+          { source: ' * It has multiple lines.' },
+        ],
+      },
+      output: ['@param {string} name This is a multiline tag.', 'It has multiple lines.'],
+    },
+    {
+      it: 'should render a multiline tag tag closes the comment',
+      input: {
+        source: [
+          { source: ' * @param {string} name This is a multiline tag.' },
+          { source: ' * It has multiple lines, and closes the comment.' },
+          { source: ' */' },
+        ],
+      },
+      output: [
+        '@param {string} name This is a multiline tag.',
+        'It has multiple lines, and closes the comment.',
+      ],
+    },
+  ];
+
+  it.each(cases)('should correctly format the case %#', (caseInfo) => {
+    // Given
+    let result = null;
+    // When
+    result = renderTagOriginal(caseInfo.input);
+    // Then
+    expect(result).toEqual(caseInfo.output);
+  });
+});
