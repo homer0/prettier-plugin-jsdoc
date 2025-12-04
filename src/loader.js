cccf688e92d19a88031664bda6da30ada76214b1
homer0 <me@homer0.dev>
2025-12-04 03:20:17 +0100

    fix: use fileURLToPath instead of __dirname

--- src/loader.js
+++ src/loader.js
@@ -1,10 +1,12 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { loadProviders } from './fns/app';
 /**
  * Loads and registers the providers of all the plugin functions.
  */
 export const loadFns = () => {
-  loadProviders(path.join(__dirname, 'fns'), [
+  const currentDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.');
+  loadProviders(path.join(currentDir, 'fns'), [
     'formatAccessTag',
     'formatArrays',
     'formatDescription',
